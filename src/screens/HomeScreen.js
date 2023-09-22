731ba8ec5b9c4541b80defe40fa7ec984af2da85
SagarBawanthade <sagar.bawanthade2004@gmail.com>
2023-09-22 22:27:09 +0530

    HomeScreen Template added && header icons Fixed

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,33 +5,40 @@ import fashionLogo from '../images/fashion logo.png';
 import mobileLogo from '../images/mobile logo.png';
 import electronicsrLogo from '../images/electronics logo.png';
 import '../styles/HomeScreen.css';
+import Homescreentemplate from '../images/Homescreentemplate.jpg';
 
 function HomeScreen() {
     return (
+        <>
 
-        <div className='container'>
-            <div className='containers'>
-                <img src={offerLogo} alt="offerLogo" />
-                <p className='heading-primary'>Top offers</p>
+            <div className='container'>
+                <div className='containers'>
+                    <a href="google.com" ><img src={offerLogo} alt="offerLogo" /></a>
+                    <p className='heading-primary'>Top offers</p>
+                </div>
+                <div className='containers'>
+                    <a href="google.com" ><img src={grocery} alt="grocery" /></a>
+                    <p className='heading-primary'>Grocery</p>
+                </div>
+                <div className='containers'>
+                    <a href="google.com" ><img src={fashionLogo} alt="fashionLogo" /></a>
+                    <p className='heading-primary'>Fashions</p>
+                </div>
+                <div className='containers'>
+                    <a href="google.com" ><img src={mobileLogo} alt="mobileLogo" /></a>
+                    <p className='heading-primary'>Mobiles</p>
+                </div>
+                <div className='containers'>
+                    <a href="google.com" ><img src={electronicsrLogo} alt="electronicsrLogo" /></a>
+                    <p className='heading-primary'>Electronics</p>
+                </div>
             </div>
-            <div className='containers'>
-                <img src={grocery} alt="grocery" />
-                <p className='heading-primary'>Grocery</p>
-            </div>
-            <div className='containers'>
-                <img src={fashionLogo} alt="fashionLogo" />
-                <p className='heading-primary'>Fashions</p>
-            </div>
-            <div className='containers'>
-                <img src={mobileLogo} alt="mobileLogo" />
-                <p className='heading-primary'>Mobiles</p>
-            </div>
-            <div className='containers'>
-                <img src={electronicsrLogo} alt="electronicsrLogo" />
-                <p className='heading-primary'>Electronics</p>
-            </div>
-        </div>
+            <div className='container2'>
+                <img src={Homescreentemplate} alt="Homescreentemplate" />
 
+            </div>
+
+        </>
     );
 }
 
